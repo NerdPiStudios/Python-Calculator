16fc21bf90f21ea3eeaa7d345216ce36de21f40a
Ayush Goyal <agoyal2001@gmail.com>
2014-12-18 14:02:56 +0530

    Add check_to_quit function

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,14 @@
 print("Welcome to NPS calculator.")
 print("Type quit at any time to exit the program.")
 print("Please enter a problem.")
+
+def check_to_quit(info):
+    try:
+        info.lower()
+    except AttributeError:
+        return
+    if info.lower() == "quit":
+        print('Now exiting...')
+        sys.exit()
+    else:
+        return
