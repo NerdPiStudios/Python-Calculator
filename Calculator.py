952d7b0c3799bde64c0b4f8ed63af55056f008f9
Ayush Goyal <agoyal2001@gmail.com>
2015-01-19 00:08:08 -0500

    Add get_two_numbers, solve_parentheses, and solve_program functions

--- Calculator.py
+++ Calculator.py
@@ -86,12 +86,288 @@ def get_problem():
     sorted(operators)
     return([digits,parentheses,operators])
 
+def get_two_numbers(number1list, number2list):
+    number1 = 0
+    number2 = 0
+    negative = False
+
+    if number1list[0] == '-':
+        del number1list[0]
+        negative = True
+
+    if '.' in number1list:
+        number1part1 = number1list[:number1list.index('.')]
+        number1part2 = number1list[number1list.index('.')+1:]
+        number1part1.reverse()
+        for i in range(0,len(number1part1)):
+            number1 = number1 + int(number1part1[i]) * (10**i)
+        for i in range(0,len(number1part2)):
+            number1 = number1 + int(number1part2[i]) * (10**(-i-1))
+    else:
+        number1list.reverse()
+        for i in range(0,len(number1list)):
+            number1 = number1 + int(number1list[i]) * (10**i)
+
+    if negative == True:
+        number1list.insert(0, '-')
+        number1 *= -1
+        negative = False
+
+    if number2list[0] == '-':
+        del number2list[0]
+        negative = True
+
+    if '.' in number2list:
+        number2part1 = number2list[:number2list.index('.')]
+        number2part2 = number2list[number2list.index('.')+1:]
+        number2part1.reverse()
+        for i in range(0,len(number2part1)):
+            number2 = number2 + int(number2part1[i]) * (10**i)
+        for i in range(0,len(number2part2)):
+            number2 = number2 + int(number2part2[i]) * (10**(-i-1))
+    else:
+        number2list.reverse()
+        for i in range(0,len(number2list)):
+            number2 = number2 + int(number2list[i]) * (10**i)
+
+    if negative == True:
+        number2list.insert(0, '-')
+        number2 *= -1
+        negative = False
+    return [number1, number2]
+
+def solve_parentheses(info):
+    digits = info[0]
+    parentheses = info[1]
+    operators = info[2]
+    parenthesesList = []
+
+    for i in parentheses:
+        parenthesesList.append(i)
+
+    while True:
+        working = []
+        parenthesesList.sort()
+        for i in range(0,len(parenthesesList)):
+            if digits[parenthesesList[i]] == ')' and digits[parenthesesList[i-1]] == '(' and i != 0:
+                working = digits[parenthesesList[i-1]+1:parenthesesList[i]]
+                currentParenthesesSection = i
+                break
+
+        while True:
+            operatorsWorking = []
+            number1list = []
+            number2list = []
+            number1 = 0
+            number2 = 0
+            currentOperator = ''
+            solution = 0
+
+            for i in range(0,len(working)):
+                if working[i] in '*+/':
+                    operatorsWorking.append(i)
+                elif working[i] == '-' and i != 0:
+                    operatorsWorking.append(i)
+
+            if len(operatorsWorking) == 1:
+                number1list = working[:operatorsWorking[0]]
+                number2list = working[operatorsWorking[0]+1:]
+                print(working)
+                print(number1list)
+                print(number2list)
+                returnedValue = get_two_numbers(number1list, number2list)
+                number1 = returnedValue[0]
+                number2 = returnedValue[1]
+
+                if working[operatorsWorking[0]] == '-':
+                    solution = number1-number2
+                elif working[operatorsWorking[0]] == '+':
+                    solution = number1+number2
+                elif working[operatorsWorking[0]] == '*':
+                    solution = number1*number2
+                elif working[operatorsWorking[0]] == '/':
+                    solution = number1/number2
+                working = []
+                for i in str(solution):
+                    working.append(i)
+
+                del digits[parenthesesList[currentParenthesesSection-1]:parenthesesList[currentParenthesesSection]+1]
+                if parenthesesList[currentParenthesesSection-1] == 0:
+                    digitsPart1 = []
+                    digitsPart2 = digits
+                elif parenthesesList[currentParenthesesSection] == len(digits)-1:
+                    digitsPart1 = digits
+                    digitsPart2 = []
+                else:
+                    digitsPart1 = digits[:parenthesesList[currentParenthesesSection-1]]
+                    digitsPart2 = digits[parenthesesList[currentParenthesesSection-1]:]
+                for i in working:
+                    digitsPart1.append(i)
+                digits = digitsPart1 + digitsPart2
+
+                parenthesesList = []
+                parentheses = {}
+                for i in range(0,len(digits)):
+                    if digits[i] == '(': # Adds to parentheses dictionary if digit is a parenthesis
+                        parentheses[i] = '('
+                        parenthesesList.append(i)
+                    elif digits[i] == ')':
+                        parentheses[i] = ')'
+                        parenthesesList.append(i)
+                break
+
+            elif len(operatorsWorking) > 1:
+                for i in range(0,len(operatorsWorking)):
+                    if working[operatorsWorking[i]] == '*' or working[operatorsWorking[i]] == '/':
+                        currentOperator = i
+                        break
+                for i in range(0,len(operatorsWorking)):
+                    if working[operatorsWorking[i]] == '+' or working[operatorsWorking[i]] == '-' and currentOperator == '':
+                        currentOperator = i
+
+                if currentOperator == 0:
+                    number1list = working[:operatorsWorking[0]]
+                    number2list = working[operatorsWorking[0]+1:operatorsWorking[1]]
+                elif currentOperator == len(operatorsWorking) - 1:
+                    number1list = working[operatorsWorking[currentOperator-1]+1:operatorsWorking[currentOperator]]
+                    number2list = working[operatorsWorking[currentOperator]+1:]
+                else:
+                    number1list = working[operatorsWorking[currentOperator-1]+1:operatorsWorking[currentOperator]]
+                    number2list = working[operatorsWorking[currentOperator]+1:operatorsWorking[currentOperator+1]]
+
+                print(working)
+                print(number1list)
+                print(number2list)
+                returnedValue = get_two_numbers(number1list, number2list)
+                number1 = returnedValue[0]
+                number2 = returnedValue[1]
+
+                if working[operatorsWorking[currentOperator]] == '-':
+                    solution = number1-number2
+                elif working[operatorsWorking[currentOperator]] == '+':
+                    solution = number1+number2
+                elif working[operatorsWorking[currentOperator]] == '*':
+                    solution = number1*number2
+                elif working[operatorsWorking[currentOperator]] == '/':
+                    solution = number1/number2
+
+                if currentOperator == 0:
+                    del working[:operatorsWorking[1]]
+                    workingPart1 = []
+                    workingPart2 = working
+                elif currentOperator == len(operatorsWorking)-1:
+                    del working[operatorsWorking[currentOperator-1]+1:]
+                    workingPart1 = working
+                    workingPart2 = []
+                else:
+                    del working[operatorsWorking[currentOperator-1]+1:operatorsWorking[currentOperator+1]]
+                    workingPart1 = working[:operatorsWorking[currentOperator-1]+1]
+                    workingPart2 = working[operatorsWorking[currentOperator-1]+1:]
+                for i in str(solution):
+                    workingPart1.append(i)
+                working = workingPart1 + workingPart2
+
+            else:
+                print(digits)
+                return digits
+
+def solve_program(info):
+    digits = info
+    while True:
+        operatorsWorking = []
+        number1list = []
+        number2list = []
+        number1 = 0
+        number2 = 0
+        currentOperator = ''
+        solution = 0
+
+        for i in range(0,len(digits)):
+            if digits[i] in '*+/':
+                operatorsWorking.append(i)
+            elif digits[i] == '-' and i != 0:
+                operatorsWorking.append(i)
+
+        if len(operatorsWorking) == 1:
+            number1list = digits[:operatorsWorking[0]]
+            number2list = digits[operatorsWorking[0]+1:]
+            returnedValue = get_two_numbers(number1list, number2list)
+            number1 = returnedValue[0]
+            number2 = returnedValue[1]
+
+            if digits[operatorsWorking[0]] == '-':
+                solution = number1-number2
+            elif digits[operatorsWorking[0]] == '+':
+                solution = number1+number2
+            elif digits[operatorsWorking[0]] == '*':
+                solution = number1*number2
+            elif digits[operatorsWorking[0]] == '/':
+                solution = number1/number2
+            digits = []
+            for i in str(solution):
+                digits.append(i)
+
+        elif len(operatorsWorking) > 1:
+            for i in range(0,len(operatorsWorking)):
+                if digits[operatorsWorking[i]] == '*' or digits[operatorsWorking[i]] == '/':
+                    currentOperator = i
+                    break
+            for i in range(0,len(operatorsWorking)):
+                if digits[operatorsWorking[i]] == '+' or digits[operatorsWorking[i]] == '-' and currentOperator == '':
+                    currentOperator = i
+
+            if currentOperator == 0:
+                number1list = digits[:operatorsWorking[0]]
+                number2list = digits[operatorsWorking[0]+1:operatorsWorking[1]]
+            elif currentOperator == len(operatorsWorking) - 1:
+                number1list = digits[operatorsWorking[currentOperator-1]+1:operatorsWorking[currentOperator]]
+                number2list = digits[operatorsWorking[currentOperator]+1:]
+            else:
+                number1list = digits[operatorsWorking[currentOperator-1]+1:operatorsWorking[currentOperator]]
+                number2list = digits[operatorsWorking[currentOperator]+1:operatorsWorking[currentOperator+1]]
+
+            returnedValue = get_two_numbers(number1list, number2list)
+            number1 = returnedValue[0]
+            number2 = returnedValue[1]
+
+            if digits[operatorsWorking[currentOperator]] == '-':
+                solution = number1-number2
+            elif digits[operatorsWorking[currentOperator]] == '+':
+                solution = number1+number2
+            elif digits[operatorsWorking[currentOperator]] == '*':
+                solution = number1*number2
+            elif digits[operatorsWorking[currentOperator]] == '/':
+                solution = number1/number2
+
+            if currentOperator == 0:
+                del digits[:operatorsWorking[1]]
+                workingPart1 = []
+                workingPart2 = digits
+            elif currentOperator == len(operatorsWorking)-1:
+                del digits[operatorsWorking[currentOperator-1]+1:]
+                workingPart1 = digits
+                workingPart2 = []
+            else:
+                del working[operatorsWorking[currentOperator-1]+1:operatorsWorking[currentOperator+1]]
+                workingPart1 = digits[:operatorsWorking[currentOperator-1]+1]
+                workingPart2 = digits[operatorsWorking[currentOperator-1]+1:]
+
+            for i in str(solution):
+                workingPart1.append(i)
+            digits = workingPart1 + workingPart2
+
+        else:
+            print(digits)
+            digits = get_two_numbers(digits, ['1'])[0]
+            print(digits)
+            return digits
+
 def mainloop():
     print('Welcome to NPS calculator.')
     print('Type quit at any time to exit the program.')
     print('Please enter a problem.')
     while True:
-        get_problem()
+        print('The answer is: ' + str(solve_program(solve_parentheses(get_problem()))))
         print('Please enter another problem or type quit to exit.')
 
 mainloop()
